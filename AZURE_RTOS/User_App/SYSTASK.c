2b6bbe5559d1b95547756d7d254b961063e17c77
王旭阳 <3353568128@qq.com>
2021-10-04 14:26:55 +0800

    updata

--- AZURE_RTOS/User_App/SYSTASK.c
+++ AZURE_RTOS/User_App/SYSTASK.c
@@ -13,13 +13,13 @@ uint64_t CPUcountMAX = 0;	//100 tick内，空闲线程运行计数最大值
 uint8_t CPUpercent = 0;		//CPU占用率百分比
 static void SYSTASK(ULONG thread_input)
 {
-	
 	IDLETaskCreate();					//最先建立空闲线程
 	CPUcount = 0;								
 	tx_thread_sleep(100);			//IDLE TASK运行100 tick，得到最大值
 	CPUcountMAX = CPUcount;
 	
-	LEDTaskCreate();
+	LEDTaskCreate();					//用户任务
+	
 	while(1)
 	{
 		CPUcount = 0;
